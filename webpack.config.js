74342411c17773570407bf5026d947be371deb8a
ahaha0807 <b5331@oic.jp>
2017-09-07 16:06:47 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        }]
+    }
+}
