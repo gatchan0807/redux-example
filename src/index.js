decd5366b088c069127272da2c62af2e16cd7353
ahaha0807 <b5331@oic.jp>
2017-09-07 18:09:16 +0900

    add comment

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ import { createStore } from 'redux'
 import { addTodo, deleteTodo } from './action'
 import { todos } from './reducer'
 
+// store の作成
 let store = createStore(todos, [])
 
+// todo の追加
 store.dispatch(addTodo("Study ES6"))
 store.dispatch(addTodo("Study redux"))
 store.dispatch(addTodo("Study react"))
 
+// todoリストの確認
 console.log(store.getState());
 
+// todo から 1 番目のものを削除
 store.dispatch(deleteTodo(1))
 
+// todoリストの確認
 console.log(store.getState());
