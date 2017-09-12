5170a04d2c697d6a1bf9f45d938c41120e67c7b2
ahaha0807 <b5331@oic.jp>
2017-09-12 10:44:28 +0900

    add test with action & reducer

---
+++ test/action.test.js
@@ -0,0 +1,26 @@
+import chai from 'chai'
+const assert = chai.assert
+import { addTodo, deleteTodo } from '../src/action'
+
+describe('アクションをテストする', () => {
+    it('ADD_TODOのアクションを返すか', () => {
+        const expectedContent = "Testing"
+        const expectedType = 'ADD_TODO'
+
+        let actual = addTodo(expectedContent)
+
+        assert.equal(actual.type, expectedType)
+        assert.equal(actual.content, expectedContent)
+    })
+
+    it('DELETE_TODOのアクションを返すか', () => {
+        const expectedIndex = 1
+        const expectedType = 'DELETE_TODO'
+
+        let actual = deleteTodo(expectedIndex)
+
+        assert.equal(actual.type, expectedType)
+        assert.equal(actual.index, expectedIndex)
+    })
+})
+
---
+++ test/reducer.test.js
@@ -0,0 +1,37 @@
+import chai from 'chai'
+const assert = chai.assert
+import { todos } from '../src/reducer'
+
+describe('リデューサーをテストする', () => {
+    let initialArray = []
+
+    beforeEach(() => {
+        initialArray = ['first todo']
+    })
+
+    it('ADD_TODOタイプでデータ追加を出来るか', () => {
+        const firstContent = initialArray[0]
+        const secondContent = 'second todo'
+        const action = {
+            type: 'ADD_TODO',
+            content: secondContent
+        }
+
+        let actual = todos(initialArray, action)
+
+        assert.equal(actual.length, 2)
+        assert.equal(actual[0], firstContent)
+        assert.equal(actual[1], secondContent)
+    })
+
+    it('DELETE_TODOタイプでデータ削除が出来るか', () => {
+        const action = {
+            type: 'DELETE_TODO',
+            index: 0
+        }
+
+        let actual = todos(initialArray, action)
+
+        assert.equal(actual.length, 0)
+    })
+})
