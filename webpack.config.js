e7fde30ea0799b67547482ef822182a3e982ada2
ahaha0807 <b5331@oic.jp>
2017-09-14 00:01:56 +0900

    デベロッパーツールにビルド前の情報を表示する設定の追加

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'bundle.js'
     },
+    devtool: 'inline-source-map',
     module: {
         loaders: [{
             exclude: /node_modules/,
