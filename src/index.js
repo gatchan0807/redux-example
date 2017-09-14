378c5ada132915d49e8ecddc26a320b657538f50
ahaha0807 <b5331@oic.jp>
2017-09-14 17:59:43 +0900

    ボタンによって各アクションを呼び出すように変更

--- src/index.js
+++ src/index.js
@@ -1,43 +1,54 @@
-import { createStore } from 'redux'
+import {createStore} from 'redux'
 import * as action from './action'
-import { todos } from './reducer'
-import { convertState2Dom, renderingDomList } from './util'
+import {todos} from './reducer'
+import {convertState2Dom, renderingDomList} from './util'
 
 // store の作成
 let store = createStore(todos, []);
 let state;
 
-// todoの追加
-store.dispatch(action.addTodo("Study ES6"));
-store.dispatch(action.addTodo("Study redux"));
-store.dispatch(action.addTodo("Study react"));
-
-state = store.getState();
-
+let todoDomList;
 let parent = document.getElementById('todo-list');
 
-// todoリストの変換・描画
-let todoDomList = convertState2Dom(state);
-renderingDomList(parent, todoDomList);
+const add = () => {
+    // todoの追加
+    store.dispatch(action.addTodo("Study ES6"));
+    store.dispatch(action.addTodo("Study redux"));
+    store.dispatch(action.addTodo("Study react"));
 
-// todoから 1 番目のものを削除
-store.dispatch(action.deleteTodo(1));
+    state = store.getState();
 
-state = store.getState();
+    // todoリストの変換・描画
+    todoDomList = convertState2Dom(state);
+    renderingDomList(parent, todoDomList);
+};
 
-// DOMを全部消す
-while (parent.firstChild) parent.removeChild(parent.firstChild);
+const deleteWithIndex = () => {
+    // todoから 1 番目のものを削除
+    store.dispatch(action.deleteTodo(1));
 
-// todoリストの変換・描画
-todoDomList = convertState2Dom(state);
-renderingDomList(parent, todoDomList);
+    state = store.getState();
 
-// todoを全て削除
-store.dispatch(action.deleteAllTodo());
+    // DOMを全部消す
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
 
-state = store.getState();
+    // todoリストの変換・描画
+    todoDomList = convertState2Dom(state);
+    renderingDomList(parent, todoDomList);
+};
 
-while (parent.firstChild) parent.removeChild(parent.firstChild);
+const allDelete = () => {
+    // todoを全て削除
+    store.dispatch(action.deleteAllTodo());
 
-todoDomList = convertState2Dom(state);
-renderingDomList(parent, todoDomList);
+    state = store.getState();
+
+    while (parent.firstChild) parent.removeChild(parent.firstChild);
+
+    todoDomList = convertState2Dom(state);
+    renderingDomList(parent, todoDomList);
+};
+
+document.querySelector('#add-button').addEventListener('click', add);
+document.querySelector('#delete-button').addEventListener('click', deleteWithIndex);
+document.querySelector('#delete-all-button').addEventListener('click', allDelete);
